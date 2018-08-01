dda3564fb3c29c1b1ed63139dec5b9526fe8ab1f
Sergey Popov <poserg@gmail.com>
2018-08-01 21:49:26 +0300

    Show absolute delta and delta in percent

--- statistics.py
+++ statistics.py
@@ -8,17 +8,19 @@ from math import ceil
 def get_delta(last_price, new_price):
     logging.debug('last_price = %s; new_price = %s', last_price, new_price)
 
-    delta = ceil(10000.0*(new_price - last_price)/last_price)/100
+    abs_delta = new_price - last_price
+    delta = ceil(10000.0*(abs_delta)/last_price)/100
     logging.debug('delta = %f', delta)
-    return delta
+    return abs_delta, delta
 
 def show_delta(delta):
-    s = str(delta) + ' %'
+    abs_delta_s = str(delta[0])
+    delta_s = str(delta[1]) + ' %'
     c = 'yellow'
-    if delta > 0:
-        s = '+' + s
+    if delta[0] > 0:
+        abs_delta_s = '+' + abs_delta_s
+        delta_s = '+' + delta_s
         c = 'green'
-    elif delta < 0:
-        s = s
+    elif delta[0] < 0:
         c = 'red'
-    print 'delta: ' + colored(s, c)
+    print 'delta: ' + colored('%s (%s)' % (abs_delta_s, delta_s), c)
